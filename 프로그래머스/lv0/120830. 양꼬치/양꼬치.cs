3934ef6538363d244f9a820bbf80b7ccb64f881f
nongdamyang <126037383+nongdamyang@users.noreply.github.com>
2023-07-17 20:29:17 +0900

    [level 0] Title: 양꼬치, Time: 0.16 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        int temp = 0;
+        
+        temp = (n / 10) * 2000;
+        answer = (n * 12000 + k * 2000) - temp;
+        
+        return answer;
+    }
+}
