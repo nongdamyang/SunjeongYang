c26d2fefba1a492eaba3583d3930de2a7fab832f
nongdamyang <126037383+nongdamyang@users.noreply.github.com>
2023-07-25 19:36:57 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.34 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] num_list) {
+        Array.Reverse(num_list);
+        
+        int[] answer = num_list;
+        
+        return answer;
+    }
+}
