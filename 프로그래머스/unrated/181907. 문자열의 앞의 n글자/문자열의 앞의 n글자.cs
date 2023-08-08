e6ac39648a5fe06f40d8f0d54e271c7154842946
nongdamyang <126037383+nongdamyang@users.noreply.github.com>
2023-08-08 20:06:35 +0900

    [unrated] Title: 문자열의 앞의 n글자, Time: 0.17 ms, Memory: 30.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string, int n) {
+        string answer = "";
+        
+        answer = my_string.Substring(0, n);
+        
+        return answer;
+    }
+}
