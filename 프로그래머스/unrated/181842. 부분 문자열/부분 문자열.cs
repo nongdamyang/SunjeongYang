142a435cfb1c737555a275df17b89b9edbfad736
nongdamyang <126037383+nongdamyang@users.noreply.github.com>
2023-08-13 21:57:45 +0900

    [unrated] Title: 부분 문자열, Time: 0.21 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(string str1, string str2) {
+        int answer = 0;
+        
+        if(str2.Contains(str1))
+        {
+            answer = 1;
+        }
+        
+        return answer;
+    }
+}
