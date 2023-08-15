5843bae5d3dbfb077e7667152e67a9e1662fd018
nongdamyang <126037383+nongdamyang@users.noreply.github.com>
2023-08-15 15:08:24 +0900

    [unrated] Title: 문자열의 뒤의 n글자, Time: 0.18 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string, int n) {
+        string answer = "";
+        
+        answer = my_string.Substring(my_string.Length - n);
+        
+        return answer;
+    }
+}
