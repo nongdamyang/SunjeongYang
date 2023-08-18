595b96fbce55ae4e758c635700197bebb8be3e4d
nongdamyang <126037383+nongdamyang@users.noreply.github.com>
2023-08-18 21:03:56 +0900

    [unrated] Title: 공백으로 구분하기 1, Time: 0.31 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public string[] solution(string my_string) {
+        string[] answer = new string[] {};
+        
+        answer = my_string.Split(" ");
+        
+        return answer;
+    }
+}
