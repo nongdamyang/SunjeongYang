a4131cdee0a39c4b78e96168ae0b859001af002b
nongdamyang <126037383+nongdamyang@users.noreply.github.com>
2023-08-27 00:20:56 +0900

    [level 0] Title: 암호 해독, Time: 0.24 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public string solution(string cipher, int code) {
+        string answer = "";
+        
+        for(int i = 0; i < cipher.Length; i++)
+        {
+            if((i + 1) % code == 0)
+            {
+                answer += cipher[i];
+            }
+        }
+        
+        return answer;
+    }
+}
