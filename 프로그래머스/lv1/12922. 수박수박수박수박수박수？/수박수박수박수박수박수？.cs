40c7a57417cccfb9769ddb5fdf3ce6343eab8a06
nongdamyang <126037383+nongdamyang@users.noreply.github.com>
2023-08-29 19:37:04 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.18 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public string solution(int n) {
+        string answer = "";
+        
+        for(int i = 1; i <= n; i++)
+        {
+            if(i % 2 != 0)
+                answer += "수";
+            else if(i % 2 == 0)
+                answer += "박";
+        }
+        
+        return answer;
+    }
+}
