1f649f44bf12caacb21b852750986f0708d19360
nongdamyang <126037383+nongdamyang@users.noreply.github.com>
2023-09-04 19:25:23 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.24 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        answer = Math.Sqrt(n) % 1 == 0 ? 1 : 2;
+        
+        return answer;
+    }
+}
