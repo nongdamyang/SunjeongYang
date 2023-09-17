e048d1d52cab08389806ad35142c883eec593b0d
nongdamyang <126037383+nongdamyang@users.noreply.github.com>
2023-09-17 20:21:05 +0900

    [level 0] Title: 가위 바위 보, Time: 1.71 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public string solution(string rsp) {
+        string answer = "";
+        
+        answer = string.Concat(rsp.Select(x => x == '2' ? '0' : x == '0' ? '5' : '2'));
+        return answer;
+    }
+}
